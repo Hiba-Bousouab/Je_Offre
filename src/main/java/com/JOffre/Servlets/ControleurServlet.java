575ea015ffcbcb15539b47b0f8d4a3b3999dcdaf
kingridda <abdelghaniridda01@gmail.com>
2021-02-17 06:27:17 +0100

    change properties

--- src/main/java/com/JOffre/Servlets/ControleurServlet.java
+++ src/main/java/com/JOffre/Servlets/ControleurServlet.java
@@ -4,11 +4,13 @@ import com.JOffre.Model.OffreModel;
 import com.JOffre.metier.OffreMetier;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/vue")
 public class ControleurServlet extends HttpServlet {
 
     private OffreMetier metier;
